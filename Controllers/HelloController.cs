759a010a2bf131835efd712e0b381326d663b4fb
Jose Luis Armas Leon <jlal92@hotmail.com>
2020-01-28 19:49:18 -0500

    Updated the View folders

--- Controllers/HelloController.cs
+++ Controllers/HelloController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Diagnostics;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using TNPCF.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -11,9 +14,13 @@ namespace TNPCF.Controllers
     public class HelloController : Controller
     {
         // GET: /<controller>/
+
         public IActionResult Index()
         {
-            return Content("<h1>Hello World</h1>", "text/html");
+            // return View();
+            return View();
+             
+            //Content("<h1>Hello World</h1>", "text/html");
         }
     }
 }
